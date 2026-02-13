07283a4b6d25563856d9f00bee059b1c89954231
webdvlpr <85422802+webdvlpr@users.noreply.github.com>
2026-02-13 19:52:24 +0530

    updated authcontext

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -66,7 +66,7 @@ async function login(req, res) {
     res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        samSite: "none",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
         path: "/"
     })
