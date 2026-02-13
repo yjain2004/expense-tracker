c5774d727116b797dc5590204dd4440f63f7d0bb
webdvlpr <85422802+webdvlpr@users.noreply.github.com>
2026-02-13 18:11:16 +0530

    Fixed case sensitive name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from './pages/login'
+import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import ProtectedRoute from './pages/Protected'
